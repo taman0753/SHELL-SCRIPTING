91e86ca7a5a34484fb751f94497a446226c3dbe2
taman0753 <taman0753@gmail.com>
2020-01-30 10:48:11 +0530

    second commit

--- palimdrome.sh
+++ palimdrome.sh
@@ -11,4 +11,24 @@ temp=$num
 
 
 while [ $num -gt 0 ] 
+do 
+	
+s=$(( $num % 10 )) 
+       
+num=$(( $num / 10 ))  
 
+rev=$(($rev*10 + $s))
+
+done
+
+if [ $temp -eq $rev ]; 
+
+then
+	
+echo "Number is palindrome"
+
+else
+	
+echo "Number is NOT palindrome"
+
+fi
